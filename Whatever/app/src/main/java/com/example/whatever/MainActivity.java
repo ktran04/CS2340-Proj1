8896428a6b66b415fb6792cc75766e729846798a
jimmyragazino <137242879+jimmyragazino@users.noreply.github.com>
2024-01-29 10:40:20 -0500

    Yippee Buttons

--- Whatever/app/src/main/java/com/example/whatever/MainActivity.java
+++ Whatever/app/src/main/java/com/example/whatever/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.whatever;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button classesButton = findViewById(R.id.classesButton);
-
+        classesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Classes.class));
+            }
+        });
     }
 
 
