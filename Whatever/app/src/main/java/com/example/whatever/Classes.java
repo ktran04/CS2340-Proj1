4d8cfd54676c6866c4851361ad77896095551a95
jimmyragazino <137242879+jimmyragazino@users.noreply.github.com>
2024-01-29 14:58:13 -0500

    Yippee

--- Whatever/app/src/main/java/com/example/whatever/Classes.java
+++ Whatever/app/src/main/java/com/example/whatever/Classes.java
@@ -2,7 +2,10 @@ package com.example.whatever;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Classes extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Classes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classes);
+        Button back = findViewById(R.id.back_to_home);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Classes.this, MainActivity.class));
+            }
+        });
+
     }
 }
