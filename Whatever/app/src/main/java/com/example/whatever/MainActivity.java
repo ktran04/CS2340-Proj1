0b684753370bb915e30c36bdd8554822457c0344
ktran04 <kytran2076@gmail.com>
2024-01-30 12:51:49 -0500

    attempted notification
    feel free to delete if it doesnt work but i think im close

--- Whatever/app/src/main/java/com/example/whatever/MainActivity.java
+++ Whatever/app/src/main/java/com/example/whatever/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.whatever;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, Classes.class));
+            }
+        });
+
+        Button notifButton = findViewById(R.id.notifButton);
+        notifButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get a reference to the Activity (Context)
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                Context context = v.getContext(); // Get context from the View
+
+                NotificationCompat.Builder myBuild = new NotificationCompat.Builder(context);
+                myBuild.setContentTitle("HIIII");
+                myBuild.setContentText("HEYYY");
+                myBuild.setSmallIcon(R.drawable.alarm_on);
+
+                NotificationManagerCompat manager = NotificationManagerCompat.from(context);
+                manager.notify(1, myBuild.build()); // Provide a notification ID
             }
         });
     }
