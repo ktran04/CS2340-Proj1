d93a866dfb5a5e514e6e7296983263c587554f12
jimmyragazino <137242879+jimmyragazino@users.noreply.github.com>
2024-01-30 13:40:06 -0500

    I Made Files We Actually Will Use

---
+++ Whatever/app/src/main/java/com/example/whatever/ToDoList.java
@@ -0,0 +1,12 @@
+package com.example.whatever;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ToDoList extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+    }
+}
