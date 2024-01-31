2e21ba9b32c6cd11c6c8c2a1d23e838e75cf5760
ktran04 <kytran2076@gmail.com>
2024-01-30 22:42:54 -0500

    WE HAVE ASSIGNMENTS
    assignments work like class

--- Whatever/app/src/main/java/com/example/whatever/Assignments.java
+++ Whatever/app/src/main/java/com/example/whatever/Assignments.java
@@ -1,14 +1,68 @@
 package com.example.whatever;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Assignments extends AppCompatActivity {
+    private Map<String, List<String>> assignmentsInfo = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignments);
+        Button back = findViewById(R.id.back_to_home);
+        Button add = findViewById(R.id.add_assignments);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Assignments.this, MainActivity.class));
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogMethod();
+            }
+        });
+    }
+
+    private void dialogMethod() {
+        LayoutInflater inflater = LayoutInflater.from(Assignments.this);
+        View dialogLayout = inflater.inflate(R.layout.enter_assignments_details, null);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(Assignments.this);
+        dialog.setView(dialogLayout);
+        TextView title = (TextView) dialogLayout.findViewById(R.id.title);
+        TextView dueDate = (TextView) dialogLayout.findViewById(R.id.dueDate);
+        TextView Class = (TextView) dialogLayout.findViewById(R.id.Class);
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                List<String> data = new ArrayList<>();
+                assignmentsInfo.put((String) title.getText(), data);
+                data.add((String) dueDate.getText());
+                data.add((String) Class.getText());
+                dialog.dismiss();
+            }
+        });
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        dialog.show();
     }
 }
--- Whatever/app/src/main/java/com/example/whatever/MainActivity.java
+++ Whatever/app/src/main/java/com/example/whatever/MainActivity.java
@@ -11,7 +11,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+//create array or object that. store objects in the array
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -25,24 +25,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Classes.class));
             }
         });
-
-        Button notifButton = findViewById(R.id.notifButton);
-        notifButton.setOnClickListener(new View.OnClickListener() {
+        Button assignmentsButton = findViewById(R.id.assignmentsButton);
+        assignmentsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Get a reference to the Activity (Context)
-                startActivity(new Intent(MainActivity.this, MainActivity.class));
-                Context context = v.getContext(); // Get context from the View
-
-                NotificationCompat.Builder myBuild = new NotificationCompat.Builder(context);
-                myBuild.setContentTitle("HIIII");
-                myBuild.setContentText("HEYYY");
-                myBuild.setSmallIcon(R.drawable.alarm_on);
-
-                NotificationManagerCompat manager = NotificationManagerCompat.from(context);
-                manager.notify(1, myBuild.build()); // Provide a notification ID
+                startActivity(new Intent(MainActivity.this, Assignments.class));
             }
         });
+
+//        Button notifButton = findViewById(R.id.notifButton);
+//        notifButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Get a reference to the Activity (Context)
+//                startActivity(new Intent(MainActivity.this, MainActivity.class));
+//                Context context = v.getContext(); // Get context from the View
+//
+//                NotificationCompat.Builder myBuild = new NotificationCompat.Builder(context);
+//                myBuild.setContentTitle("HIIII");
+//                myBuild.setContentText("HEYYY");
+//                myBuild.setSmallIcon(R.drawable.alarm_on);
+//
+//                NotificationManagerCompat manager = NotificationManagerCompat.from(context);
+//                manager.notify(1, myBuild.build()); // Provide a notification ID
+//            }
+//        });
     }
 
 
