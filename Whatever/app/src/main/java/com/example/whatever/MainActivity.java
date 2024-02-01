1d5c987a101e5ddfedd3f1aa6514403774da92b8
jimmyragazino <137242879+jimmyragazino@users.noreply.github.com>
2024-02-01 13:38:02 -0500

    YIPPEEEEEE

--- Whatever/app/src/main/java/com/example/whatever/MainActivity.java
+++ Whatever/app/src/main/java/com/example/whatever/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button classesButton = findViewById(R.id.classesButton);
         classesButton.setOnClickListener(new View.OnClickListener() {
             @Override
