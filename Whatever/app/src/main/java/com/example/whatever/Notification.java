8e0249990cb6dea96470a05cabd96c6f8ad7bb8c
ktran04 <kytran2076@gmail.com>
2024-02-02 19:00:01 -0500

    changed todo
    AAAAAA

---
+++ Whatever/app/src/main/java/com/example/whatever/Notification.java
@@ -0,0 +1,4 @@
+package com.example.whatever;
+
+public class Notification {
+}
--- Whatever/app/src/main/java/com/example/whatever/ToDoList.java
+++ Whatever/app/src/main/java/com/example/whatever/ToDoList.java
@@ -1,68 +1,106 @@
 package com.example.whatever;
 
-import android.content.Context;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
-public class ToDoList extends AppCompatActivity {
-    private ArrayList<String> items;
-    private ArrayAdapter<String> itemsAdapter;
-    private ListView listView;
-    private Button addButton;
+public class ToDoList<ItemEntry> extends AppCompatActivity {
+    int selected;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+        selected = -1;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.to_do);
-
-        listView = findViewById(R.id.listView);
-        addButton = findViewById(R.id.addToDoButton);
-
-        addButton.setOnClickListener(new View.OnClickListener() {
+        Button back = findViewById(R.id.back_to_home);
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addItem(v);
+                startActivity(new Intent(ToDoList.this, MainActivity.class));
             }
         });
 
-        items = new ArrayList<>();
-        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
-        listView.setAdapter(itemsAdapter);
-        setUpViewListener();
+        ListView toDoListView = (ListView) findViewById(R.id.todo_list);
+        ArrayList<String> backingList = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, backingList);
+        toDoListView.setAdapter(adapter);
 
-    }
-    private void setUpViewListener() {
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        EditText item = findViewById(R.id.toDoItem);
+
+        Button addItem = findViewById(R.id.todo_add);
+        addItem.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long l) {
-                Context context = getApplicationContext();
-                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                String itemStr = item.getText().toString();
 
-                items.remove(i);
-                itemsAdapter.notifyDataSetChanged();
-                return true;
+                backingList.add((itemStr));
+                adapter.notifyDataSetChanged();
+
+                item.setText("");
+            }
+        });
+
+        Button edit = findViewById(R.id.todo_edit);
+        toDoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selected = position;
+                String now;
+                now = backingList.get(position);
+
+                item.setText(now);
+
+                Toast.makeText(ToDoList.this, now + " has been selected to be edited or deleted.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selected != -1) {
+                    String now = "";
+                    now = backingList.get(selected);
+                    now = (item.getText().toString());
+
+                    adapter.notifyDataSetChanged();
+
+                    item.setText("");
+                    selected = -1;
+                }
+            }
+        });
+
+        Button del = findViewById(R.id.todo_delete);
+        del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selected != -1) {
+                    backingList.remove(selected);
+                    adapter.notifyDataSetChanged();
+                    selected = -1;
+
+                    item.setText("");
+                }
             }
         });
     }
-    private void addItem(View v) {
-        EditText input = findViewById(R.id.editTextText);
-        String itemText = input.getText().toString();
-
-        if(!(itemText.equals(""))) {
-            itemsAdapter.add(itemText);
-            input.setText("");
-        } else {
-            Toast.makeText(getApplicationContext(), "Please enter something", Toast.LENGTH_LONG).show();
-        }
-    }
-
-
 }
